554c04ba3bad9cdc4519e2151d9ee02efe012aa5
Kei Fujikawa <fujikei22funk@gmail.com>
2021-07-04 22:48:27 +0900

    colorも追加

--- lib/count_page.dart
+++ lib/count_page.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:riverpods_sample/color_model.dart';
 import 'package:riverpods_sample/count_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final model = context.read(counterProvider);
+    final colorModel = context.read(colorProvider);
+    final countModel = context.read(counterProvider);
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: colorModel.changeColor,
+            icon: Icon(Icons.update),
+          )
+        ],
+      ),
       body: Center(
         child: Consumer(builder: (context, watch, child) {
           final count = watch(counterProvider).counter;
+          final color = watch(colorProvider).currentColor;
           return Text(
             '$count',
-            style: TextStyle(fontSize: 56),
+            style: TextStyle(
+              fontSize: 80,
+              color: color,
+            ),
           );
         }),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: model.incrementCounter,
+        onPressed: countModel.incrementCounter,
         child: Icon(Icons.add),
       ),
     );
